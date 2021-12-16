22668850a116359f36a945b83db4fd9010aca985
takamotobiz <takamotobiz@users.noreply.github.com>
2021-12-17 06:49:08 +0900

    main

--- selenium.py
+++ selenium.py
@@ -1,13 +1,10 @@
-﻿# -*- coding: utf-8 -*-
-
-from selenium import webdriver
+﻿from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
 import tkinter as tk
 from tkinter import messagebox
 
 options = webdriver.ChromeOptions()
-# options.add_argument('--user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 [FBAN/FBIOS;FBDV/iPhone13,2;FBMD/iPhone;FBSN/iOS;FBSV/14.4.2;FBSS/3;FBID/phone;FBLC/en_US;FBOP/5]')
 options.add_argument(' --incognito')
 
 # create chrome driver
@@ -27,22 +24,9 @@ driver.execute_cdp_cmd(
 driver.get('https://www.google.co.jp/search?q=ランチ&ei=Yhw7XdqhNdn6-Qb_5bvABg&start=0&sa=N&ved=0ahUKEwja2o6v89LjAhVZfd4KHf_yDmg4FBDy0wMIdQ&biw=1920&bih=910')
 
 # scroll footer
-# 画面スクロールが不要であれば下記2行を削除してください。
-# element = driver.find_element_by_id("sfooter")
-# driver.execute_script("arguments[0].scrollIntoView(false);", element)
+element = driver.find_element_by_id("sfooter")
+driver.execute_script("arguments[0].scrollIntoView(false);", element)
 
 # hide root window
 root = tk.Tk()
 root.withdraw()
-# show dialog( elapsed time(S) )
-messagebox.showinfo("確認", "\"現在地を更新\"をクリックしOKボタンをクリックしてください")
-
-# add case - quit Chrome
-try:
-    html = driver.page_source
-    with open('001.html', 'w', encoding='utf-8') as f:
-        f.write(html)
-
-    driver.quit()
-except:
-    pass
